7bca5f7f029f2264eb5d2eaa922a0b58ef795053
Rafael de Paula <xspynks@gmail.com>
2022-11-28 09:14:07 -0300

    feat: add counter, timer, modify css details

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
-// get the click event from the element with id "btn1" and call the function "addText"
-document.getElementById("btn1").addEventListener("click", addText);
-
-function addText() {
-  document.getElementById("second-box").innerHTML = "Hello World";
-}
+// get the click event from the buttons and call the respective functions
+const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2");
+const btn3 = document.getElementById("btn3");
+let counter = 0;
+let counterDiv = document.getElementById("counter");
+let timerDiv = document.getElementById("timer");
 
 // insert the text "Hello World" in the element with id "second-container"
+btn1.addEventListener("click", () => {
+  document.getElementById("second-box").innerHTML = "Hello World";
+});
+
+// count the number of times the button is clicked and display the count in the element with id "counter"
+// for every click, add one second to the timer
+btn2.addEventListener("click", () => {
+  counter++;
+  timerDiv.innerHTML = counter;
+});
+
+// when "btn3" is clicked, insert a timer in the element with id "timer" that counts that count from 0 to 1000
+btn3.addEventListener("click", () => {
+  let timerInterval = setInterval(() => {
+    if (counter <= 0) {
+      clearInterval(timerInterval);
+    }
+    console.log(`${counter} segundos`);
+    timerDiv.innerText = counter--;
+  }, 1000);
+});
